1edcb50d9e8c24e72b3858242ff9e836b186d498
shila <sheelabhusal567@gmail.com>
2026-01-25 08:48:40 +0545

    Added frontend

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,23 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\JobDetailsController;
+use App\Http\Controllers\JobListController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TestimonialController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -16,5 +28,22 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/',[HomeController::class,'index'])->name('home');
+
+Route::get('/aboutus',[AboutController::class,'index'])->name('aboutus');
+
+Route::get('/contact',[ContactController::class,'index'])->name('contact');
+
+Route::get('/category',[CategoryController::class,'index'])->name('category');
+
+
+
+Route::get('/job_details',[JobDetailsController::class,'index'])->name('job_details');
+
+Route::get('/job_list',[JobListController::class,'index'])->name('job_list');
+
+Route::get('/testimonials',[TestimonialController::class,'index'])->name('testimonials');
+
 
 require __DIR__.'/auth.php';
